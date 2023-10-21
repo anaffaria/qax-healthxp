e8c06d7c99ee323bbc8dfe4582e2809d4b65c0d1
anaffaria <anacarolinq.ng@gmail.com>
2023-10-21 17:36:17 -0300

    added the new scenario

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,29 @@
-import user from '../fixtures/users.json'
+import users from "../fixtures/users.json";
 
 describe("login", () => {
-  it("Deve logar com o perfil do admin", () => {
 
+  
+  it("Deve logar com o perfil do admin", () => {
+    const user = users.admin;
     cy.log(JSON.stringify(user));
 
     cy.visit("http://localhost:3000");
+
     cy.get("input[name=email]").type(user.email);
     cy.get("input[name=password]").type(user.password);
     cy.contains("button", "Entrar").click();
+
     cy.contains("aside .logged-user", "Olá, " + user.name).should("be.visible");
+  });
+
+  it("Não deve logar com senha incorreta", () => {
+    const user = users.inv_pass
+
+    cy.visit("http://localhost:3000");
+
+    cy.get("input[name=email]").type(user.email);
+    cy.get("input[name=password]").type(user.password);
+    cy.contains("button", "Entrar").click();
+
   });
 });
