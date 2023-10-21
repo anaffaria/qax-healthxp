597aa98edfb46d6b9bb682b5ffbe43932b9b177e
anaffaria <anacarolinq.ng@gmail.com>
2023-10-21 17:14:49 -0300

    added the fixtures in the login.cy

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,14 @@
-describe('login', ()=>{
-    it('Deve logar com o perfil do admin', ()=>{
-        
-        
-        const user ={
-            email: 'admin@healthxp.com',
-            password: 'xperience'
-        }
+import user from '../fixtures/users.json'
 
-        cy.visit('http://localhost:3000')
-        cy.get('input[name=email]').type(user.email)
-        cy.get('input[name=password]').type(user.password)
-        cy.contains('button', 'Entrar').click()
-        cy.contains('aside .logged-user', 'Olá, Admin')
-            
+describe("login", () => {
+  it("Deve logar com o perfil do admin", () => {
 
+    cy.log(JSON.stringify(user));
 
-    })
-})
+    cy.visit("http://localhost:3000");
+    cy.get("input[name=email]").type(user.email);
+    cy.get("input[name=password]").type(user.password);
+    cy.contains("button", "Entrar").click();
+    cy.contains("aside .logged-user", "Olá, " + user.name).should("be.visible");
+  });
+});
