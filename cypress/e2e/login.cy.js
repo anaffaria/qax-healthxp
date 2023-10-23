03a5172a7f2964ead396caaf70d7430afcdb4695
anaffaria <anacarolinq.ng@gmail.com>
2023-10-23 20:44:14 -0300

    learning about fixtures, commands and pages

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,29 +1,49 @@
 import users from "../fixtures/users.json";
+import login from "../support/pages/LoginPage";
+
 
 describe("login", () => {
-
-  
   it("Deve logar com o perfil do admin", () => {
     const user = users.admin;
-    cy.log(JSON.stringify(user));
 
-    cy.visit("http://localhost:3000");
-
-    cy.get("input[name=email]").type(user.email);
-    cy.get("input[name=password]").type(user.password);
-    cy.contains("button", "Entrar").click();
+    login.submit(user)
 
     cy.contains("aside .logged-user", "Olá, " + user.name).should("be.visible");
   });
 
   it("Não deve logar com senha incorreta", () => {
-    const user = users.inv_pass
+    const user = users.inv_pass;
 
-    cy.visit("http://localhost:3000");
+    login.submit(user)
+    login.popUpHave("Suas credenciais são inválidas, por favor tente novamente!");
+  });
 
-    cy.get("input[name=email]").type(user.email);
-    cy.get("input[name=password]").type(user.password);
-    cy.contains("button", "Entrar").click();
+  it("Não deve logar com e-mail não cadastrado", () => {
+    const user = users.email_not_found;
 
+    login.submit(user)
+    login.popUpHave("Suas credenciais são inválidas, por favor tente novamente!");
+  });
+
+  it("Não deve logar com e-mail invalido", () => {
+    const user = users.inv_email;
+
+    login.submit(user)
+    login.popUpHave("Insira um email válido.");
+  });
+
+  it("Não deve logar com e-mail em branco", () => {
+    const user = users.empty_email;
+
+    login.submit(user)
+    login.popUpHave("Os campos email e senha são obrigatórios.");
+  });
+
+  it("Não deve logar com senha em branco", () => {
+    const user = users.empty_password;
+
+    login.submit(user)
+    login.popUpHave("Os campos email e senha são obrigatórios.");
   });
 });
+
